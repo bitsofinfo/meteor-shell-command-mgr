982feba1583cfe2149519e3ce274412cbe4bc37d
bitsofinfo <bitsofinfo.g@gmail.com>
2015-02-16 14:14:47 -0700

    latest work

--- powershell-meteor.js
+++ powershell-meteor.js
@@ -9,7 +9,7 @@ if (Meteor.isClient) {
   Session.set('argumentFormErrors',[]);
   Session.set('arguments',{});
 
-  Template.commandForm.helpers({
+  Template.commandFormBody.helpers({
       commandFormErrors: function() {
         return Session.get('commandFormErrors');
       }
@@ -28,20 +28,49 @@ if (Meteor.isClient) {
   });
 
   // @see https://github.com/meteor/meteor/issues/2194
-  Template.argumentsForm.helpers({
-      arguments: function () {
-        var toReturn = [];
-        var args = Session.get('arguments');
-        for (key in args) {
-          var argObj = args[key];
-          toReturn.push(argObj);
+  Template.argumentsTable.helpers({
+
+      arguments: function (loadArgumentsFrom, commandId) {
+
+        if (loadArgumentsFrom == 'session') {
+
+          var toReturn = [];
+          var args = Session.get('arguments');
+          for (key in args) {
+            var argObj = args[key];
+            toReturn.push(argObj);
+          }
+          return toReturn;
+
+
+        } else {
+          return Arguments.find({'commandId':commandId}, {sort : {createdAt:-1}});
         }
-        return toReturn;
+
       }
   });
 
 
-  Template.commandForm.events({
+  Template.commandEditorModal.helpers({
+
+    currentEditCommand: function (mode, commandId) {
+
+        var currentEditCommand = Session.get('currentEditCommand');
+
+        if (typeof(currentEditCommand) != 'undefinded' && currentEditCommand) {
+          return currentEditCommand;
+
+        } else {
+          return {};
+        }
+
+      }
+  });
+
+
+
+
+  Template.commandFormBody.events({
 
 
     // Send the command and arguments to the server
@@ -72,104 +101,8 @@ if (Meteor.isClient) {
           form.returnType.value = "";
 
           Session.set('arguments',{});
-          Session.set('errors',[]);
-        }
-
-      });
-
-      // Prevent default form submit
-      return false;
-    },
-
-
-
-    // add a new argument to the command (no persistence)
-    // just adds an argument to the session
-    "click #btnAddArgument": function (event) {
-
-      Session.set('argumentFormErrors',[]);
-
-      $('#ctrlArgumentName').parent().removeClass('has-feedback has-error');
-      $('#ctrlDefaultValue').parent().removeClass('has-feedback has-error');
-
-      var template = Template.instance();
-      var argumentName = template.find('#ctrlArgumentName').value;
-      var defaultValue = template.find('#ctrlDefaultValue').value;
-      var isQuoted = template.find('#ctrlIsQuoted').checked;
-      var isValued = template.find('#ctrlIsValued').checked;
-
-      var hasErrors = false;
-
-      if (argumentName.trim().length == 0) {
-        $('#ctrlArgumentName').parent().addClass('has-feedback has-error');
-        var errors = Session.get('argumentFormErrors');
-        errors.push({'msg':'argument name is required'});
-        Session.set('argumentFormErrors',errors);
-        hasErrors = true;
-      }
-
-      if (!isValued && defaultValue.trim().length > 0) {
-        $('#ctrlDefaultValue').parent().addClass('has-feedback has-error');
-        var errors = Session.get('argumentFormErrors');
-        errors.push({'msg':'is valued is not checked, yet default value is defined!'});
-        Session.set('argumentFormErrors',errors);
-        hasErrors = true;
-      }
-
-      if (hasErrors) {
-        return false;
-      }
-
-      var arguments = Session.get('arguments');
-
-      var argId = Random.id();
-
-      arguments[argId] = {
-                        '_id': argId,
-                        'argumentName':argumentName,
-                        'defaultValue':defaultValue,
-                        'isQuoted':isQuoted,
-                        'isValued':isValued
-                        };
-
-      Session.set('arguments',arguments);
-
-      template.find('#ctrlArgumentName').value = '';
-      template.find('#ctrlDefaultValue').value = '';
-      template.find('#ctrlIsQuoted').checked = false;
-      template.find('#ctrlIsValued').checked = false;
-
-      // Prevent default form submit
-      return false;
-    },
-
-
-    // add a new argument to the command (no persistence)
-    // just removes an argument by id from session
-    "click .btnRemoveArgument": function (event) {
-
-      var toRemoveId = event.target.id;
-      var arguments = Session.get('arguments');
-
-      delete arguments[toRemoveId];
-
-      Session.set('arguments',arguments);
-
-      // Prevent default form submit
-      return false;
-    },
-
-
-    // add a new argument to the command (no persistence)
-    // just removes an argument by id from session
-    "click .btnRemoveCommand": function (event) {
-
-      var toRemoveId = event.target.id;
-
-      Meteor.call("deleteCommand", toRemoveId, function(error,result) {
-
-        if (error) {
-          Session.set('errors',[{'msg':error.message,'stack':error.stack}]);
+          Session.set('commandFormErrors',[]);
+          Session.set('argumentFormErrors',[]);
         }
 
       });
@@ -178,8 +111,93 @@ if (Meteor.isClient) {
       return false;
     }
 
+  });
+
+
+
+  Template.argumentsForm.events({
+
+
+      // add a new argument to the command (no persistence)
+      // just adds an argument to the session
+      "click #btnAddArgument": function (event) {
+
+        Session.set('argumentFormErrors',[]);
+
+        $('#ctrlArgumentName').parent().removeClass('has-feedback has-error');
+        $('#ctrlDefaultValue').parent().removeClass('has-feedback has-error');
+
+        var template = Template.instance();
+        var argumentName = template.find('#ctrlArgumentName').value;
+        var defaultValue = template.find('#ctrlDefaultValue').value;
+        var isQuoted = template.find('#ctrlIsQuoted').checked;
+        var isValued = template.find('#ctrlIsValued').checked;
+
+        var hasErrors = false;
+
+        if (argumentName.trim().length == 0) {
+          $('#ctrlArgumentName').parent().addClass('has-feedback has-error');
+          var errors = Session.get('argumentFormErrors');
+          errors.push({'msg':'argument name is required'});
+          Session.set('argumentFormErrors',errors);
+          hasErrors = true;
+        }
+
+        if (!isValued && defaultValue.trim().length > 0) {
+          $('#ctrlDefaultValue').parent().addClass('has-feedback has-error');
+          var errors = Session.get('argumentFormErrors');
+          errors.push({'msg':'is valued is not checked, yet default value is defined!'});
+          Session.set('argumentFormErrors',errors);
+          hasErrors = true;
+        }
+
+        if (hasErrors) {
+          return false;
+        }
+
+        var arguments = Session.get('arguments');
+
+        var argId = Random.id();
+
+        arguments[argId] = {
+                          '_id': argId,
+                          'argumentName':argumentName,
+                          'defaultValue':defaultValue,
+                          'isQuoted':isQuoted,
+                          'isValued':isValued
+                          };
+
+        Session.set('arguments',arguments);
+
+        template.find('#ctrlArgumentName').value = '';
+        template.find('#ctrlDefaultValue').value = '';
+        template.find('#ctrlIsQuoted').checked = false;
+        template.find('#ctrlIsValued').checked = false;
+
+        // Prevent default form submit
+        return false;
+      },
+
+
+      // add a new argument to the command (no persistence)
+      // just removes an argument by id from session
+      "click .btnRemoveArgument": function (event) {
+
+        var toRemoveId = event.target.id;
+        var arguments = Session.get('arguments');
+
+        delete arguments[toRemoveId];
+
+        Session.set('arguments',arguments);
+
+        // Prevent default form submit
+        return false;
+      }
+
 
   });
+
+
 
   Template.commandsList.events({
 
@@ -188,19 +206,53 @@ if (Meteor.isClient) {
     // just removes an argument by id from session
     "click .btnRemoveCommand": function (event) {
 
-      var toRemoveId = event.target.id;
+        bootbox.confirm('Are you sure you want to delete this command?', function(result){
 
-      Meteor.call("deleteCommand", toRemoveId, function(error,result) {
+          if (result) {
+            var toRemoveId = event.target.id;
 
-        if (error) {
-          Session.set('errors',[{'msg':error.message,'stack':error.stack}]);
-        }
+            Meteor.call("deleteCommand", toRemoveId, function(error,result) {
 
-      });
+              if (error) {
+                Session.set('errors',[{'msg':error.message,'stack':error.stack}]);
+              }
 
-      // Prevent default form submit
-      return false;
-    }
+            });
+
+            return true;
+          }
+
+        });
+
+      },
+
+      // launches edit command modal
+      "click .btnEditCommand": function (event) {
+
+          $("#commandFormPanelBody").collapse('hide');
+          Session.set('arguments',{});
+          Session.set('commandFormErrors',[]);
+          Session.set('argumentFormErrors',[]);
+          $('#commandName').parent().removeClass('has-feedback has-error');
+          $('#command').parent().removeClass('has-feedback has-error');
+
+          var toEditId = event.target.id;
+          var command = Commands.findOne({'_id':toEditId});
+          var argCursor = Arguments.find({'commandId':toEditId}, {sort : {createdAt:-1}});
+          var arguments = argCursor.fetch();
+
+          var sessionArgs = {};
+          for (var i=0; i<arguments.length; i++) {
+            var argObj = arguments[i];
+            sessionArgs[argObj._id] = argObj;
+          }
+
+
+          Session.set('currentEditCommand',command);
+          Session.set('arguments',sessionArgs);
+
+        },
+
 
 
   });
@@ -264,7 +316,7 @@ Meteor.methods({
 
   addArgument: function (commandId, argumentName, defaultValue, isQuoted, isValued) {
 
-    var command = Commands.findOne({commandId: commandId});
+    var command = Commands.findOne({'_id': commandId});
     if (typeof(command) == 'undefined') {
       throw new Meteor.Error("cmd-not-found", "addArgument() command " + commandId + " not found");
     }

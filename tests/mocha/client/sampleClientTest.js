2648dd8947f75547fe513f116a533f096fa4dcf9
bitsofinfo <bitsofinfo.g@gmail.com>
2015-02-17 15:56:52 -0700

    start on client

--- tests/mocha/client/sampleClientTest.js
+++ tests/mocha/client/sampleClientTest.js
@@ -1,8 +1,19 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
-    describe("a group of tests", function(){
-      it("should respect equality", function(){
-        chai.assert.equal(5,5);
+
+    describe("basic ui test", function(){
+
+      it("should have command in list", function(){
+
+        Meteor.call("saveCommand", null, 'testCommand1', 'command1', 'none', null);
+        var commands = Commands.find().fetch();
+        chai.expect(commands.length).to.equal(1);
+
+        var x= $("#commandsListContainer").find("td").first().text();
+        console.log(x);
+        chai.assert.equal(x, 'testCommand1');
+
+        
       });
     });
   });
